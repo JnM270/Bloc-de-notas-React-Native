77097fd742a4d820609a54b0c2d31935a6127ba5
JnM270 <98226304+JnM270@users.noreply.github.com>
2025-05-25 15:10:44 -0400

    Actualizado

--- Componentes/EditarNotas.js
+++ Componentes/EditarNotas.js
@@ -1,4 +1,4 @@
- import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert } from 'react-native'; 
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert } from 'react-native'; 
 import { useState } from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
