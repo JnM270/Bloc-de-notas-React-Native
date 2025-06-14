806c8f129e96f4f47c085c32dcc193f0a1e390a8
JnM270 <98226304+JnM270@users.noreply.github.com>
2025-06-14 13:47:52 -0400

    Actualización final

--- Componentes/Inicio.js
+++ Componentes/Inicio.js
@@ -1,75 +1,78 @@
-import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native"; 
 
-const Inicio = ({ navigation }) => {
-  return (
-    <View style={styles.container}>
+
+const Inicio = ({ navigation }) => { 
+  return ( 
+    <View style={styles.container}> 
       {/* Logo de la app*/}
-      <View style={styles.logoContainer}>
-        <Image 
-          source={require("../assets/logo1.png")}
-          style={styles.image}
-        />
-      </View>
+      <View style={styles.logoContainer}> 
+        <Image source={require("../assets/logo001.png")} style={styles.image} /> 
+      </View> 
+      
+      {/* Título */}
+      <Text style={styles.title}>Bienvenido a Eco del Bosque</Text> 
 
-      <Text style={styles.title}>¡Bienvenido a Amazing Note!</Text>
+      {/* Botones */}
+      <View style={styles.buttonContainer}> 
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Registro")}> 
+          <Text style={styles.buttonText}>Registrarse</Text> 
+        </TouchableOpacity> 
 
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Registro")}>
-          <Text style={styles.buttonText}>Registrarse</Text>
-        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Login")}> 
+          <Text style={styles.buttonText}>Iniciar sesión</Text> 
+        </TouchableOpacity> 
+      </View> 
+    </View> 
+  ); 
+}; 
 
-        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Login")}>
-          <Text style={styles.buttonText}>Iniciar sesión</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
-};
+const styles = StyleSheet.create({ 
+  container: { 
+    flex: 1, 
+    justifyContent: "center", 
+    alignItems: "center", 
+    backgroundColor: "#c5d1b5", 
+  }, 
 
-const styles = StyleSheet.create({
-   container: {
-     flex: 1,
-     justifyContent: "center",
-     alignItems: "center",
-     backgroundColor: "#f8f9fa",
-   },
-   logoContainer: {
+  logoContainer: { 
     marginBottom: 20, 
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  image: {
-    width: 240,
-    height: 240,
-    resizeMode: "contain",
-  },
-   title: {
-     fontSize: 28,
-     fontWeight: "bold",
-     marginBottom: 20,
-   },
-   buttonContainer: {
-     flexDirection: "row",
-     gap: 10,
-   },
-   button: {
-     backgroundColor: "#008000",
-     paddingVertical: 12,
-     paddingHorizontal: 24,
-     borderRadius: 5,
- 
-   },
-   buttonText: {
-     color: "#fff",
-     fontSize: 18,
-     fontFamily: "Verdana, Geneva, Tahoma, sans-serif",
-   },
-   registerText: {
-    position: 18,
-    fontSize: 16,
-    color: "#007bff",
-    textDecorationLine: "underline",
-   },
- });
- 
- export default Inicio;
+    justifyContent: "center", 
+    alignItems: "center", 
+  }, 
+  image: { 
+    width: 240, 
+    height: 240, 
+    resizeMode: "contain", 
+  }, 
+  title: { 
+    fontSize: 28,  
+    fontWeight: "bold", 
+    fontFamily: "RobotoBold", 
+    marginBottom: 20, 
+    color: "#0d3900", 
+  }, 
+  buttonContainer: { 
+    flexDirection: "column", 
+    alignItems: "center", 
+    gap: 15, 
+  }, 
+  button: { 
+    backgroundColor: "#0d3900", 
+    paddingVertical: 12, 
+    paddingHorizontal: 24, 
+    borderRadius: 15, 
+  }, 
+  buttonText: { 
+    color: "#fff", 
+    fontSize: 18, 
+    fontFamily: "RobotoRegular", 
+  }, 
+  registerText: { 
+    fontSize: 16, 
+    fontFamily: "RobotoRegular", 
+    color: "#007bff", 
+    textDecorationLine: "underline", 
+  }, 
+}); 
+
+export default Inicio;
